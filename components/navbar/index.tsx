ad9e72d81d6c4fc9f9727c51922adf3ec697bee3
Karthi <karthikthee7@gmail.com>
2024-06-26 18:33:49 +0530

    logo correction

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
-import { LockKeyhole } from 'lucide-react';
-import { UserRound } from 'lucide-react';
-import { Logo } from "@/public/images";
+import { LockKeyhole } from "lucide-react";
+import { UserRound } from "lucide-react";
+import Image from "next/image";
+// import { Logo } from "@/public/images";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="w-full grid items-center h-24 shadow-inner">
+      <nav className="w-full grid items-center h-24 shadow-inner ">
         <div className="w-full flex items-center justify-around">
           <Link href={"/"}>
             <div className="flex gap-2">
-              <div className="w-20">
-                <img
+              <div className="w-32 relative">
+                <Image
                   className="w-full h-full"
-                  src={`${Logo}`}
+                  src="/images/training academy logo.png"
                   alt="logo"
+                  fill={true}
                 />
               </div>
               <h3>
@@ -31,16 +33,20 @@ const Navbar = () => {
             <li>About RSR</li>
             <li>Stakeholders</li>
           </ul>
-          <div className="flex gap-2">
-          <button className="py-2 px-4 font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-3xl relative bottom-3 flex text-white gap-1">
-          <LockKeyhole /> <span>Sign up</span>
-          </button>
-          <button className="py-2 px-4 font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-3xl relative bottom-3 flex text-white gap-1">
-          <LockKeyhole /> Sign in
-          </button>
-
+          <div className="flex gap-2 items-center">
+            <Link
+              href="/signup"
+              className="py-2 px-4 font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-3xl relative bottom-3 flex text-white gap-1"
+            >
+              <LockKeyhole /> <span>Sign up</span>
+            </Link>
+            <Link
+              href="/login"
+              className="py-2 px-4 font-bold bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-3xl relative bottom-3 flex text-white gap-1"
+            >
+              <LockKeyhole /> Sign in
+            </Link>
           </div>
-         
         </div>
       </nav>
     </>
