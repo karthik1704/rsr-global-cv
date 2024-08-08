7937690c1a09d380155a7821f89a31e0be634bbe
Karthi <karthikthee7@gmail.com>
2024-08-08 13:15:45 +0530

    so

--- next.config.mjs
+++ next.config.mjs
@@ -35,12 +35,12 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: process.env.NEXT_PUBLIC_APP_URL,
+            value:  `${process.env.NEXT_PUBLIC_APP_URL}`,
           },
           
           {
             key: 'X-Forwarded-Host',
-            value: process.env.NEXT_PUBLIC_APP_URL,
+            value: `${process.env.NEXT_PUBLIC_APP_URL}`,
           },
           
         ],
