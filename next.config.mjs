d2effc25f7709102f27550d5c4382144d67b2bc1
Karthi <karthikthee7@gmail.com>
2024-08-08 13:45:50 +0530

    new fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'react-pdf.org',
-            port: '',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "react-pdf.org",
+        port: "",
       },
-      eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true, // i will change in future (adding valid types)
-      },
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-      },
-     experimental: {
-      serverActions: {
-        serverActions: { allowedOrigins: ["3.110.169.239","3.110.169.239:3000", "3.110.169.239:3002"], }
-
-      },
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true, // i will change in future (adding valid types)
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "3.110.169.239",
+        "3.110.169.239:3000",
+        "3.110.169.239:3002",
+      ],
     },
-   
- 
+  },
 };
 
 export default nextConfig;
