f1551899ab4a3d44aabe9cc3b47504d5d45e9694
Karthi <karthikthee7@gmail.com>
2024-08-08 11:42:27 +0530

    local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,39 @@
 import type { Metadata } from "next";
-import { Poppins} from "next/font/google";
+// import { Poppins} from "next/font/google";
+import localFont  from 'next/font/local'
 import "./globals.css";
 
 import { cookies } from "next/headers";
 
 
 
-export const poppins = Poppins({
-  style:['normal'],
-  display: 'swap', 
-  adjustFontFallback: false,
-  subsets:['latin'],
-  weight:['400','700'],
-  variable:'--font-poppins',
+// export const poppins = Poppins({
+//   style:['normal'],
+//   display: 'swap', 
+//   adjustFontFallback: false,
+//   subsets:['latin'],
+//   weight:['400','700'],
+//   variable:'--font-poppins',
   
+// })
+
+const poppins = localFont({
+  src: [
+    {
+      path: 'fonts/Poppins/Poppins-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    
+    },
+    {
+      path: 'fonts/Poppins/Poppins-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+   
+  ],
+  variable:'--font-poppins',
+
 })
 
 
