7b49e3628710d276039b87e7757464cab2b9ad25
Karthi <karthikthee7@gmail.com>
2024-08-21 22:24:09 +0530

    new change

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,3 @@
 // export const SERVER_API_URL = 'http://localhost:8000';
-export const SERVER_API_URL = 'http://13.201.194.87:8444';
+// export const SERVER_API_URL = 'http://13.201.194.87:8444';
+export const SERVER_API_URL = 'http://api.rsrglobalresumebuilder.com';
