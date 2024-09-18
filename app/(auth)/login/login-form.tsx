a22376a1fe4fb72d3eb6533ee0a73a71c2dd08ec
Gobiga Shree <gobigashreesm@gmail.com>
2024-09-18 11:15:35 +0530

    fix-design-home

--- app/(auth)/login/login-form.tsx
+++ app/(auth)/login/login-form.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Image from "next/image";
-import {toast} from 'sonner';
+import { toast } from "sonner";
 import Link from "next/link";
 import { signinJwt } from "./actions";
 import { useEffect } from "react";
 import { useFormState } from "react-dom";
-import img from '@/public/images/rsr_logo-1.png'
+import img from "@/public/images/rsr_logo-1.png";
 
 const initalState = {
   message: null,
@@ -14,8 +14,6 @@ const initalState = {
     password: null,
   },
 };
-
-
 
 const LoginForm = () => {
   const [state, formAction] = useFormState(signinJwt, initalState);
@@ -30,87 +28,86 @@ const LoginForm = () => {
 
   return (
     // <html className={`${poppins.variable}`}>
-    <div className= " min-h-screen flex flex-col justify-center items-center bg-slate-100">
-      <div className=" w-full flex justify-center">
-      
-      <div className="bg-[url('/sign-bg-1.jpg')] bg-cover bg-center w-4/12 p-8 bg-white shadow-lg text-justify">
-      <h1 className="text-4xl text-gray-100 font-bold mb-4 text-center ">
-        Sign In
-      </h1>
-        <form action={formAction}>
-          <div className="mb-4 mx-12">
-            <label className="block text-gray-100 font-bold">
-              Enter your e-mail address
-            </label>
-            <div className="relative">
-            <input
-              type="text"
-              name="username"
-              className="px-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
-            />
-           
+    <div className=" min-h-screen flex flex-col justify-center items-center py-10 bg-slate-100">
+      <div className=" w-full flex flex-col lg:flex-row items-center justify-center">
+        <div className="bg-[url('/sign-bg-1.jpg')] bg-cover bg-center w-4/5 sm:w-3/5 md:w-7/12 lg:w-4/12 p-8 bg-white shadow-lg text-justify mb-6 lg:mb-0">
+          <h1 className="text-4xl text-gray-100 font-bold mb-4 text-center ">
+            Sign In
+          </h1>
+          <form action={formAction}>
+            <div className="mb-4 mx:4 md:mx-8 lg:mx-12">
+              <label className="block text-gray-100 font-bold">
+                Enter your e-mail address
+              </label>
+              <div className="relative">
+                <input
+                  type="text"
+                  name="username"
+                  className="px-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
+                />
+              </div>
+              {state?.fieldErrors?.username && (
+                <p className="text-red-500">{state?.fieldErrors?.username}</p>
+              )}
             </div>
-            {state?.fieldErrors?.username && (
-          <p className="text-red-500">{state?.fieldErrors?.username}</p>
-        )}
-
-          </div>
-          <div className="mb-4 mx-12">
-            <label className="block text-gray-100 font-bold">
-              Enter your password
-            </label>
-            <div className="relative">
-            <input
-              type="password"
-              name="password"
-              className="px-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
-            />
-            
-
+            <div className="mb-4 mx:4 md:mx-8 lg:mx-12">
+              <label className="block text-gray-100 font-bold">
+                Enter your password
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  name="password"
+                  className="px-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
+                />
+              </div>
+              {state?.fieldErrors?.password && (
+                <p className="text-red-500">{state?.fieldErrors?.password}</p>
+              )}
             </div>
-            {state?.fieldErrors?.password && (
-          <p className="text-red-500">{state?.fieldErrors?.password}</p>
-        )}
-
-          </div>
-          <div className="flex justify-center">
-          {/* <Link
+            <div className="flex justify-center">
+              {/* <Link
             href="/signup"
             className="text-green-600 hover:text-green-500 underline text-sm mx-12 mb-6 font-bold"
           >
             Create an account
           </Link> */}
-          <button
-            type="submit"
-           className="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Login
-          </button>
-          </div>
+              <button
+                type="submit"
+                className="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
 
-        <div className="flex ml-64">
-          {/* <label>
+            <div className="flex ml-64">
+              {/* <label>
         <input
           type="checkbox"
         />
         Remember Me
       </label> */}
-            {/* <h1>forgot Password?</h1> */}
+              {/* <h1>forgot Password?</h1> */}
+            </div>
+          </form>
+        </div>
+
+        <div className="w-4/5 sm:w-3/5 md:w-7/12 lg:w-4/12 bg-slate-200">
+          <div className="flex flex-col justify-center items-center py-16">
+            <Image src={img} alt="logo"></Image>
+            <h1 className="text-black text-4xl font-bold my-2">
+              Welcome to Login
+            </h1>
+            <p className="text-black my-2">Don't have an account?</p>
+            <Link
+              href="/signup"
+              className="flex w-24 justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </Link>
           </div>
-        </form>
-</div>
-
-<div className=" w-4/12 bg-slate-200">
-
-<div className="flex flex-col justify-center items-center mt-10">
-<Image src={img} alt="logo"></Image>
-  <h1 className="text-black text-4xl font-bold my-2">Welcome to Login</h1>
-  <p className="text-black my-2">Don't have an account?</p>
-  <Link href="/signup" className="flex w-24 justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</Link>
-</div>
-</div>
-
-</div>
+        </div>
+      </div>
 
       {/* <p className="text-left m-6">
         Easy, fast and secure: download the{" "}
@@ -130,9 +127,8 @@ const LoginForm = () => {
         </li>
       </ul> */}
     </div>
-      // </html> 
+    // </html>
   );
 };
 
 export default LoginForm;
-
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,41 +1,46 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import Image from "next/image";
-import img from '@/public/home-1.png'
-import ImageCarousel from '@/components/resume/index'
-import Link from 'next/link';
+import img from "@/public/home-1.png";
+import ImageCarousel from "@/components/resume/index";
+import Link from "next/link";
 
- const Header = ({user}) => {
-    const images = [
-        '/resume/resume-1.jpg',
-        '/resume/resume-2.jpg',
-        '/resume/resume-3.jpg'
-      ];
+const Header = ({ user }) => {
+  const images = [
+    "/resume/resume-1.jpg",
+    "/resume/resume-2.jpg",
+    "/resume/resume-3.jpg",
+  ];
 
-  return<>
-<header className="h-[600px] w-full  bg-cover bg-center bg-no-repeat relative">
-<Image src={img} alt="" />
-<div className='absolute top-3/4 right-1/4 left-1/3'>
-<ImageCarousel images={images} interval={2000} />
-</div>
-    <div className='absolute top-1/3 left-10 text-black w-3/5 flex flex-col gap-4 justify-start items-start'>
-        <h2 className='text-4xl font-bold '>
-            The CV that gets the job... Done
-        </h2>
-        <p className='text-lg'> Perfect Your CV Effortlessly, Much More than a CV generator</p>
+  return (
+    <>
+      <header className="h-[600px] w-full bg-cover bg-center bg-no-repeat relative">
+        <Image src={img} alt="" className="w-full h-full object-cover" />
 
-        <div>
-        <Link href={user? '/resume':'/login' } className='text-xl	py-2 px-5 rounded-md outline-1 outline outline-offset-0 outline-white text-white bg-green-600 text-md font-bold hover:bg-green-500 hover:text-slate-100 transition-colors hover:ease-in-out'>
-            Create your CV
-        </Link>
-        {/* <button className=' text-xl mx-5 py-2 px-5 rounded-md outline-1 outline outline-offset-0 outline-black text-black bg-white text-md font-bold hover:bg-green-600 hover:outline-none hover:text-slate-100 transition-colors hover:ease-in-out'>
-           Upgrade a CV
-        </button> */}
+        <div className="absolute top-3/4 right-1/4 left-1/3 sm:left-1/4 sm:right-1/4 sm:top-auto sm:bottom-0 md:left-1/5 lg:left-1/3 hidden lg:block">
+          <ImageCarousel images={images} interval={2000} />
         </div>
 
-    </div>
-</header>
-  </>
-}
+        <div className="absolute top-1/3 left-4 text-black w-full sm:w-4/5 md:w-3/5 lg:w-2/6 xl:w-3/5 flex flex-col gap-4 justify-start items-start px-4 md:px-10">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">
+            The CV that gets the job... Done
+          </h2>
+          <p className="text-base sm:text-lg md:text-xl">
+            Perfect Your CV Effortlessly, Much More than a CV generator
+          </p>
+
+          <div>
+            <Link
+              href={user ? "/resume" : "/login"}
+              className="text-base sm:text-lg md:text-xl py-2 px-4 md:px-5 rounded-md outline outline-1 outline-offset-0 outline-white text-white bg-green-600 font-bold hover:bg-green-500 hover:text-slate-100 transition-colors ease-in-out"
+            >
+              Create your CV
+            </Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default Header;
