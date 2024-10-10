a90630dc7a02e7e75d4647badc5da428eac3313c
Karthi <karthikthee7@gmail.com>
2024-10-10 12:54:12 +0530

    fix

--- app/constants.ts
+++ app/constants.ts
@@ -1,8 +1,8 @@
 // export const SERVER_API_URL = 'http://18.169.220.66:7744';
 // export const SERVER_API_URL = 'http://api.rsrglobalresumebuilder.com';
-// export const SERVER_API_URL = 'https://api.rsrglobalresumebuilder.com';
-// export const PAYMENT_SUCCESS_URL = 'https://rsrglobalresumebuilder.com/payment/pay/success';
+export const SERVER_API_URL = 'https://api.rsrglobalresumebuilder.com';
+export const PAYMENT_SUCCESS_URL = 'https://rsrglobalresumebuilder.com/payment/pay/success';
 
-export const SERVER_API_URL = 'http://localhost:8000';
-export const PAYMENT_SUCCESS_URL = 'http://localhost:3000/payment/pay/success';
+// export const SERVER_API_URL = 'http://localhost:8000';
+// export const PAYMENT_SUCCESS_URL = 'http://localhost:3000/payment/pay/success';
 
