01054858d52fa36efb15f3f5ee46fd14f33ea205
Karthi A <karthikthee7@gmail.com>
2024-10-25 17:37:01 +0530

    Refactor redirect path in navbar component

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -46,7 +46,7 @@ export async function getData() {
   console.log(user);
   
   if(user.expiry_date && !lessThanExpiryDate(user.expiry_date)){
-    redirect ('/payment/renew');
+    redirect ('/payment');
   }
 
   return user;
