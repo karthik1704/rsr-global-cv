c9aa71d52fe6711d2bd42eaf361504c2889d7afe
jenish798 <solomonjenish@gmail.com>
2024-11-22 17:08:12 +0530

    disable

--- app/(root)/resume/[id]/forms/education.tsx
+++ app/(root)/resume/[id]/forms/education.tsx
@@ -333,7 +333,7 @@ const handleFromDateChange = (index: number, value: string) => {
                         max={getCurrentDate()}
                         className="px-4 block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
                         type="Date"
-                        disabled={!fromDates[index]}
+                        disabled={false}
                       />
                       {errors.educations?.[index]?.to_date && (
                         <p className="text-red-700 text-sm">
