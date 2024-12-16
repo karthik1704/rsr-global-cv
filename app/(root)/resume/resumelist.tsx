a30a8b8414026d21d9e1d29f96c4fcd5f05eacb0
Karthi A <karthikthee7@gmail.com>
2024-12-16 10:50:13 +0530

    updated packages

--- app/(root)/resume/resumelist.tsx
+++ app/(root)/resume/resumelist.tsx
@@ -88,24 +88,23 @@ const ResumeList = ({ resumes, user }: Props) => {
               <div className="w-full mx-auto transition-transform transform hover:scale-105 bg-white shadow-lg rounded-lg overflow-hidden">
                 <div className="p-4 md:p-6 flex flex-col lg:flex-row justify-evenly">
                   <div className="text-gray-600 space-y-2 flex-col items-center">
-                  {user && user.first_name && user.last_name
-                          ?(
-                    <p className="flex items-center space-x-2">
-                      <span>
-                        <UserCheck color="#121d91" />
-                      </span>
-                      <span className="pl-3 lg:text-lg text-base font-bold text-black capitalize space-x-2">
-                        { `${user.first_name} ${user.last_name}`}
-                      </span>
-                    </p>
-                          ): null}
+                    {user && user.first_name && user.last_name ? (
+                      <p className="flex items-center space-x-2">
+                        <span>
+                          <UserCheck color="#121d91" />
+                        </span>
+                        <span className="pl-3 lg:text-lg text-base font-bold text-black capitalize space-x-2">
+                          {`${user.first_name} ${user.last_name}`}
+                        </span>
+                      </p>
+                    ) : null}
 
                     <p className="lg:text-lg text-base text-black flex items-center space-x-2">
                       <span>
                         <CircleCheckBig color="#32b34c" />
                       </span>
                       <span className=" pl-3 lg:text-lg text-base text-black flex items-center space-x-2">
-                      Joined: {dateFormatter(user.created_at)}
+                        Joined: {dateFormatter(user.created_at)}
                       </span>
                     </p>
                   </div>
@@ -124,42 +123,54 @@ const ResumeList = ({ resumes, user }: Props) => {
                       <span className="pl-3 text-black">{user.email}</span>
                     </p>
                     {resumes && resumes[0].country ? (
-                    <p className="flex space-x-2">
-                      <span>
-                        <MapPinCheck />
-                      </span>
-                      <span className="pl-3 text-black">
-                       {`${resumes[0].country}`}
-                      </span>
-                    </p>
-                     ): null}
+                      <p className="flex space-x-2">
+                        <span>
+                          <MapPinCheck />
+                        </span>
+                        <span className="pl-3 text-black">
+                          {`${resumes[0].country}`}
+                        </span>
+                      </p>
+                    ) : null}
                   </div>
                   {resumes && resumes[0] && (
-  <div className="text-gray-600 space-y-2 flex-col items-center">
-    <p className="flex space-x-2 flex-col items-start">
-      {resumes[0].address_line_1 || resumes[0].address_line_2 || resumes[0].city || resumes[0].postal_code ? (
-        <span className="flex items-center space-x-2">
-          {resumes[0].address_line_1 && <MapPin />}
-          <span className="pl-3 text-black">
-            {resumes[0].address_line_1 ? `${resumes[0].address_line_1}` : null}
-            {resumes[0].address_line_1 && (
-              <>
-                {resumes[0].address_line_2 || resumes[0].city || resumes[0].postal_code ? ',' : ''}
-              </>
-            )}
-          </span>
-        </span>
-      ) : null}
+                    <div className="text-gray-600 space-y-2 flex-col items-center">
+                      <p className="flex space-x-2 flex-col items-start">
+                        {resumes[0].address_line_1 ||
+                        resumes[0].address_line_2 ||
+                        resumes[0].city ||
+                        resumes[0].postal_code ? (
+                          <span className="flex items-center space-x-2">
+                            {resumes[0].address_line_1 && <MapPin />}
+                            <span className="pl-3 text-black">
+                              {resumes[0].address_line_1
+                                ? `${resumes[0].address_line_1}`
+                                : null}
+                              {resumes[0].address_line_1 && (
+                                <>
+                                  {resumes[0].address_line_2 ||
+                                  resumes[0].city ||
+                                  resumes[0].postal_code
+                                    ? ","
+                                    : ""}
+                                </>
+                              )}
+                            </span>
+                          </span>
+                        ) : null}
 
-      <span className="pl-9 text-black">
-        {resumes[0].address_line_2 && `${resumes[0].address_line_2}`}{resumes[0].address_line_2 ? ', ' : ''}
-        {resumes[0].city && `${resumes[0].city}`}{resumes[0].city ? ', ' : ''}
-        {resumes[0].postal_code && `${resumes[0].postal_code}`}
-      </span>
-    </p>
-  </div>
-)}
-
+                        <span className="pl-9 text-black">
+                          {resumes[0].address_line_2 &&
+                            `${resumes[0].address_line_2}`}
+                          {resumes[0].address_line_2 ? ", " : ""}
+                          {resumes[0].city && `${resumes[0].city}`}
+                          {resumes[0].city ? ", " : ""}
+                          {resumes[0].postal_code &&
+                            `${resumes[0].postal_code}`}
+                        </span>
+                      </p>
+                    </div>
+                  )}
                 </div>
               </div>
 
@@ -176,12 +187,12 @@ const ResumeList = ({ resumes, user }: Props) => {
                       </p>
                     </div>
                     <Link
-                        href={`/resume/${resume.id}`}
-                        className="lg:ml-5 ml-5 flex lg:w-24 w-14 h-10 justify-center items-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        <span className="hidden lg:block">Edit</span>
-                        <Pencil size={20} className='block lg:hidden'/>
-                      </Link>
+                      href={`/resume/${resume.id}`}
+                      className="lg:ml-5 ml-5 flex lg:w-24 w-14 h-10 justify-center items-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      <span className="hidden lg:block">Edit</span>
+                      <Pencil size={20} className="block lg:hidden" />
+                    </Link>
                   </div>
 
                   <div className="p-10 flex">
@@ -197,7 +208,10 @@ const ResumeList = ({ resumes, user }: Props) => {
                   <div className="p-10 flex">
                     <WalletCards className="text-green-700" size={32} />
                     <p className="pl-3 flex flex-col md:flex-row lg:text-lg text-base font-bold text-black">
-                      Payment:<span className="lg:text-lg text-base font-normal ml-1">Done</span>
+                      Payment:
+                      <span className="lg:text-lg text-base font-normal ml-1">
+                        Done
+                      </span>
                     </p>
                   </div>
                 </div>
